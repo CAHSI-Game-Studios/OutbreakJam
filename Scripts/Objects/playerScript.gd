778cd84a8f1d199e8e54a882de1139df44253997
Saul-Caban-UPRM <93611806+Saul-Caban-UPRM@users.noreply.github.com>
2025-03-05 15:56:06 -0400

    Added a next Room and Stim logic for double jump, dash, and, floating.

--- Scripts/Objects/playerScript.gd
+++ Scripts/Objects/playerScript.gd
@@ -6,8 +6,9 @@ extends CharacterBody2D
 
 const SPEED : int = 300.0
 const JUMP_VELOCITY : int = -400.0
+var dashSpeed: int = 3000
 
-
+var Falling_position: float = 0
 # IMPLEMENT LOGIC TO CHANGE selectedStim maybe a keypressed -1/+1 the value
 var heldStims : Array[Stim]
 var selectedStim : int
@@ -15,7 +16,7 @@ var decay : int
 
 # Stim variables
 var canDash : bool = false
-var canDoubleJump : bool = false
+var canDoubleJump : bool = true
 var isMagnetic : bool = false
 var isFloaty : bool = false
 
@@ -27,7 +28,12 @@ var notRunning : bool = true
 var fastFall : float = 1
 var direction : float
 
-
+# Use stim variables
+var canUseDoubleJump : bool = true
+# Used Floating stim
+var usedFloaty: bool = false
+# Dash state variables
+var isDashing: bool = false
 # Runs everytime the player is instantiated
 func _ready():
 	animation_tree.active = true
@@ -36,26 +42,58 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	update_animation_parameters()
 	input()
-	if not is_on_floor():
-		velocity.y += get_gravity().y * delta * fastFall
-	velocity.x = SPEED*direction*running
+	if isDashing:
+		isDashing = false
+	else:
+		if not is_on_floor():
+			
+			velocity.y += get_gravity().y * delta * fastFall
+		
+		velocity.x = SPEED*direction*running
+	if Input.is_action_just_pressed("use_item") && canDash:
+		Dash()
+	if isDashing:
+		velocity.x = dashSpeed * direction
 	move_and_slide()
 
 # Handles the input 
 func input():
 	if Input.is_action_pressed("down"):
 		fastFall = 1.5
-	else:
+	
+	print(is_on_floor())
+	print(fastFall)
+	if is_on_floor():
 		fastFall = 1
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		jumped = true
+		if usedFloaty:
+			isFloaty = false
+			isFloaty = false
+	if Input.is_action_just_pressed("use_item") && isFloaty:
+		if(velocity.y >0):
+			fastFall = 0.05
+			usedFloaty = true
+		
+	
+		
+	
+	if Input.is_action_just_pressed("jump"):
+		if  is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			jumped = true
+			canUseDoubleJump = true
+			Falling_position = velocity.y
+		elif canDoubleJump && canUseDoubleJump:
+			velocity.y = JUMP_VELOCITY
+			canUseDoubleJump = false
+			canDoubleJump = false
+			Falling_position = velocity.y 
 	direction = Input.get_axis("left","right")
 	if Input.is_action_pressed("run") && direction:
 		running = 1.25
 	else:
 		running = 1
-
+	
+		
 # See Player->AnimationTree. https://www.youtube.com/watch?v=iElHZhOxGYA&ab_channel=Bitlytic
 func update_animation_parameters():
 	if velocity.x < 0:
@@ -92,16 +130,41 @@ func update_animation_parameters():
 		
 	
 func useStim() -> void:
+	if heldStims.is_empty():
+		return
+	
 	var stimToUse : Stim = heldStims[selectedStim]
 	match stimToUse.getType():
 		1: 
-			canDash = true
+			applyDashStim()
 		2:
-			canDoubleJump = true
+			applyDoubleJumpStim()
 		3:
-			isFloaty = true
+			applyFloatyStim()
 		4: 
-			isMagnetic = true
+			applyMagneticStim()
 	# remove stim from the ui
 	decay -= stimToUse.getDecayCost()
 	heldStims.erase(stimToUse)
+
+func applyDashStim() -> void:
+	canDash = true
+	
+
+func applyDoubleJumpStim() -> void:
+	canDoubleJump = true
+	
+
+func applyFloatyStim() -> void:
+	isFloaty = true
+	
+
+func applyMagneticStim() -> void:
+	isMagnetic = true
+	
+func Dash() -> void:
+	isDashing = true
+	canDash = false
+	
+	
+	
