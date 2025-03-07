fc2181f9223e25d1058763f3e9dbf3e8e177098b
yadzielperez09 <yadziel.perez@upr.edu>
2025-03-06 20:12:06 -0400

    added the spikes

--- Scripts/Objects/jump_pad.gd
+++ Scripts/Objects/jump_pad.gd
@@ -1,12 +1,13 @@
 extends Node2D
 #author Yadziel
 @onready var force = 500
+@onready var JP_Animation: AnimatedSprite2D = $AnimatedSprite2D
 
 func _on_area_2d_body_entered(body: Player) -> void:
-	print("me cago en luma")
-	print(rotation_degrees)
-	print("y " + str(sin(rotation)*force))
-	print("x " + str(cos(rotation)*force))
+	JP_Animation.play("default")
 	var new_v = Vector2(cos(rotation)*force,sin(rotation)*force)
 	body.velocity += new_v
-	
+
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if JP_Animation.frame == 0:
+		JP_Animation.pause()
